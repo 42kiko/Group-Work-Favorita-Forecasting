71a0663b16034b864ce49ec58d57bec94987054f
42kiko <kiko97@tuta.io>
2026-01-14 13:42:32 +0100

    add pandasai parquet upload and limit 1gb

--- pandasai_app/app.py
+++ pandasai_app/app.py
@@ -7,10 +7,15 @@ llm = LocalLLM(api_base="http://localhost:11434/v1", model="llama3")
 
 st.title("Data analysis with PandasAI")
 
-uploaded_file = st.file_uploader("Upload a CSV file", type=["csv"])
+uploaded_file = st.file_uploader(
+    "Upload a CSV or Parquet file", type=["parquet", "csv"]
+)
 
 if uploaded_file is not None:
-    data = pd.read_csv(uploaded_file)
+    if uploaded_file.name.endswith(".parquet"):
+        data = pd.read_parquet(uploaded_file)
+    else:
+        data = pd.read_csv(uploaded_file)
     st.write(data.head(3))
 
     df = SmartDataframe(data, config={"llm": llm})
